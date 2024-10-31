99574f85575a615b6f05a2b1eda5068ef7eef380
Miguel Tavares <migueloliveiradev@gmail.com>
2024-10-30 22:26:24 -0300

    feat: basic UI

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
+use fake::faker::name::raw::*;
+use fake::{locales::*, Fake};
 use iced::padding::{bottom, left, top};
 use iced::widget::{column, container, horizontal_rule, row, text, vertical_rule};
 use iced::Alignment::Center;
 use iced::Length::Fill;
 use iced::{color, Element};
 
-pub fn rem_to_px(rem: f32) -> f32 {
-    rem * 16.
-}
-
 pub fn main() -> iced::Result {
-    iced::application("MuChat", Application::update, Application::view)
+    iced::application("muchat", Application::update, Application::view)
         .theme(Application::theme)
         .run()
 }
@@ -26,49 +24,47 @@ enum Message {
 }
 
 impl Application {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
-    }
+    fn update(&mut self, message: Message) {}
 
     fn view(&self) -> Element<Message> {
-        Element::from(column![row![
-            container(
-                column![
-                    container(text("Chats").size(20))
-                        .align_x(Center)
-                        .align_y(Center)
-                        .height(48)
-                        .width(Fill),
-                    container(horizontal_rule(0.5)).padding(bottom(16)),
-                    column![
-                        text("Miguel"),
-                        text("Hello World!").size(10).color(color!(0xA1A1A1))
-                    ]
-                    .spacing(4)
-                    .padding(left(48))
-                    .width(Fill),
-                    container(horizontal_rule(0.5)).padding(top(16))
-                ]
-                .width(300)
-            ),
-            container(vertical_rule(0.5)).height(Fill),
-            container("No selected chat")
-                .align_x(Center)
-                .align_y(Center)
-                .width(Fill)
-                .height(Fill)
-        ]])
-        // .explain(color!(0xff0000))
+        home()
     }
 
     fn theme(&self) -> iced::Theme {
         iced::Theme::CatppuccinMocha
     }
+}
+
+fn home<'a>() -> Element<'a, Message> {
+    let random_name: String = Name(EN).fake();
+
+    let sidebar = container(
+        column![
+            container(text("Chats").size(20))
+                .align_x(Center)
+                .align_y(Center)
+                .height(48)
+                .width(Fill),
+            container(horizontal_rule(0.5)).padding(bottom(16)),
+            column![
+                text(random_name),
+                text("Hello World!").size(10).color(color!(0xA1A1A1))
+            ]
+            .spacing(4)
+            .padding(left(48))
+            .width(Fill),
+            container(horizontal_rule(0.5)).padding(top(16))
+        ]
+        .width(300),
+    );
+
+    let divider = container(vertical_rule(0.5)).height(Fill);
+
+    let basic_text = container("No selected chat")
+        .align_x(Center)
+        .align_y(Center)
+        .width(Fill)
+        .height(Fill);
+
+    column![row![sidebar, divider, basic_text]].into()
 }
